797bbb6bcc154c116e2d0c9697d09145e2fce810
thisishwan2 <feel000617@gmail.com>
2023-06-25 23:40:47 +0900

    [Feat] Main 생성

---
+++ backend/src/main/java/com/example/GANerate/GaNerateApplication.java
@@ -0,0 +1,13 @@
+package com.example.GANerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaNerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaNerateApplication.class, args);
+	}
+
+}
