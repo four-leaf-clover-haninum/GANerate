78a2b2c74495eab67e869002b582033eddc9b77c
thisishwan2 <feel000617@gmail.com>
2023-07-02 18:07:37 +0900

    [FEAT] 예외 처리

---
+++ backend/src/main/java/com/example/GANerate/enumuration/Result.java
@@ -0,0 +1,37 @@
+package com.example.GANerate.enumuration;
+
+import lombok.Getter;
+
+@Getter
+public enum Result {
+
+    OK(0, "성공"),
+    LOGOUT_OK(0, "로그아웃 성공"),
+    DELETE_OK(0, "회원 탈퇴 성공"),
+    FAIL(-1, "실패"),
+    NOT_USES_TOKEN(-2, "유효한 토큰이 존재하지 않습니다."),
+
+    //유저
+    USERID_DUPLICATED(2200, "중복되는 아이디"),
+    USERID_NOT_FOUND(2201, "존재하지 않는 아이디"),
+    INVALID_PASSWORD(2202, "올바르지 않은 비밀번호"),
+    NOT_FOUND_USER(2203, "존재하지 않는 회원");
+
+    private final int code;
+    private final String message;
+
+    Result(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Result resolve(int code) {
+        for (Result result : values()) {
+            if (result.getCode() == code) {
+                return result;
+            }
+        }
+        return null;
+    }
+
+}
