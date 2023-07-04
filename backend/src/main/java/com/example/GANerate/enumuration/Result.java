392799492eac35435d65ef1e41fe96646b2637f3
thisishwan2 <feel000617@gmail.com>
2023-07-04 20:52:53 +0900

    [FEAT] 에러 코드 추가

--- backend/src/main/java/com/example/GANerate/enumuration/Result.java
+++ backend/src/main/java/com/example/GANerate/enumuration/Result.java
@@ -15,7 +15,10 @@ public enum Result {
     USERID_DUPLICATED(2200, "중복되는 아이디"),
     USERID_NOT_FOUND(2201, "존재하지 않는 아이디"),
     INVALID_PASSWORD(2202, "올바르지 않은 비밀번호"),
-    NOT_FOUND_USER(2203, "존재하지 않는 회원");
+    NOT_FOUND_USER(2203, "존재하지 않는 회원"),
+    UNAUTHORITY_TOKEN(2204, "권한 정보가 없는 토큰");
+
+    //
 
     private final int code;
     private final String message;
