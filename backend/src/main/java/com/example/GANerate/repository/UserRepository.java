1f7f88e53499c2ec1f1ba4d2d955a3bd7be236c2
thisishwan2 <feel000617@gmail.com>
2023-07-04 20:44:18 +0900

    [FEAT] repository 생성

--- backend/src/main/java/com/example/GANerate/repository/UserRepository.java
+++ backend/src/main/java/com/example/GANerate/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.GANerate.repository;
 
 import com.example.GANerate.domain.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Boolean existsByEmail(String userEmail);
+    @EntityGraph(attributePaths = "authorities") // email로 user 조회할때 authorities도 같이 조회
+    Optional<User> findOneWithAuthoritiesByEmail(String email);
 }
