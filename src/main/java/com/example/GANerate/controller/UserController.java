3b6aacde59839c4ab49b6aeeccf338548c9db952
thisishwan2 <feel000617@gmail.com>
2023-07-05 20:34:25 +0900

    [FEAT] 이메일 인증 컨트롤러 추가

---
+++ src/main/java/com/example/GANerate/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.example.GANerate.controller;
+
+import com.example.GANerate.request.UserRequest;
+import com.example.GANerate.response.CustomResponseEntity;
+import com.example.GANerate.response.UserResponse;
+import com.example.GANerate.service.EmailService;
+import com.example.GANerate.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final EmailService emailService;
+
+    //회원가입
+    @PostMapping("/auth/signup")
+    public CustomResponseEntity<UserResponse.signup> signup(
+            @RequestBody @Valid final UserRequest.signup request){
+        return CustomResponseEntity.success(userService.signup(request));
+    }
+
+    //이메일 전송
+    @PostMapping("/auth/email")
+    public CustomResponseEntity<String> sendEmail(@RequestBody @Valid final UserRequest.emailAuth request) {
+        return CustomResponseEntity.success(emailService.authEmail(request));
+    }
+
+    //이메일 인증번호 인증
+    @GetMapping("/auth/email")
+    public CustomResponseEntity<String> checkEmailNum(@RequestBody @Valid final UserRequest.emailNum request) {
+        return CustomResponseEntity.success(emailService.checkNum(request));
+    }
+
+    //로그인
+    @PostMapping("/auth/signin")
+    public CustomResponseEntity<UserResponse.signin> signin(
+            @RequestBody @Valid final UserRequest.signin request){
+        return CustomResponseEntity.success(userService.signin(request));
+    }
+
+    //로그아웃
+
+
+    //유저 정보 조회
+    @GetMapping("/auth/users")
+    public CustomResponseEntity<List<UserResponse.userAll>> findUsers(){
+        return CustomResponseEntity.success(userService.findAll());
+    }
+
+    //프로필 수정
+
+
+    //좋아요 상품 조회
+
+
+    //구매 상품 조회
+
+
+
+
+}
