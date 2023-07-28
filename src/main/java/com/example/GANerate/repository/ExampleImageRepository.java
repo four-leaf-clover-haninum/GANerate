6b1c213e0d0c766d8c0dd53a751d310d3ce89383
thisishwan2 <feel000617@gmail.com>
2023-07-28 16:01:11 +0900

    [FEAT] JPA 레포지토리 생성

---
+++ src/main/java/com/example/GANerate/repository/ExampleImageRepository.java
@@ -0,0 +1,7 @@
+package com.example.GANerate.repository;
+
+import com.example.GANerate.domain.ExampleImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExampleImageRepository extends JpaRepository<ExampleImage, Long> {
+}
