3696caa0395b3610e37ca627b62ca604181e14bd
thisishwan2 <feel000617@gmail.com>
2023-07-28 15:57:36 +0900

    [FEAT] JPA 리포지토리 생성

---
+++ src/main/java/com/example/GANerate/repository/ZipFileRepository.java
@@ -0,0 +1,7 @@
+package com.example.GANerate.repository;
+
+import com.example.GANerate.domain.ZipFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZipFileRepository extends JpaRepository<ZipFile, Long> {
+}
