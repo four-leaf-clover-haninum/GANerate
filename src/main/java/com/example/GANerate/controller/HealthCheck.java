117f22bc5099ff7f62fdb67b409968ddfdb2fb20
thisishwan2 <feel000617@gmail.com>
2023-07-31 01:28:49 +0900

    [FEAT] health checking 용 컨트롤러

---
+++ src/main/java/com/example/GANerate/controller/HealthCheck.java
@@ -0,0 +1,21 @@
+package com.example.GANerate.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HealthCheck {
+
+    @Value("${version}")
+    private String version;
+
+    @GetMapping("/")
+    public String version() {
+        return String.format("Project Version : %s", version);
+    }
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+
+}
