ad346dc2c4f49e5945cce7567287950449227116
thisishwan2 <feel000617@gmail.com>
2023-07-31 00:14:52 +0900

    [FEAT] 결과 코드 추가

--- src/main/java/com/example/GANerate/enumuration/Result.java
+++ src/main/java/com/example/GANerate/enumuration/Result.java
@@ -21,11 +21,25 @@ public enum Result {
     FAIL_SEND_EMAIL(2205, "인증번호 전송 실패"),
     UNCORRECT_CERTIFICATION_NUM(2206, "올바르지 않은 인증번호"),
     INVALID_REFRESH_TOKEN(2207, "올바르지 않은 refresh token"),
-    UN_AUTHENTICATION_EMAIL(2208, "이메일 미인증")
-    ;
+    UN_AUTHENTICATION_EMAIL(2208, "이메일 미인증"),
+
+    // 데이터 상품
+    NOT_FOUND_DATA_PRODUCT(3001, "존재하지 않는 데이터 상품"),
+
+    //파일업로드
+    INVALID_FILE(4001, "jpg, png 파일만 zip내부에 있을 수 있음."),
+    FAIL_UPLOAD_FILE(4002, "파일 업로드 실패"),
+    NO_IMAGE_FILE(4003,"zip 안에 이미지 파일이 존재하지 않음"),
+    INCORRECT_FILE_TYPE_Only_JPG_JPEG_PNG(4004, "올바르지 않은 파일 확장자"),
+
+    //좋아요
+    DUPLICATED_HEART(5001, "좋아요 중복"),
+
+    //카테고리
+    NON_EXIST_CATEGORY(6001, "존재하지 않는 카테고리");
 
 
-    //
+
 
     private final int code;
     private final String message;
