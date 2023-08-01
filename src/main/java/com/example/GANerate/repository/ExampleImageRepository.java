7ca0866f345b8039257663ea45c7a8ebffa3017c
thisishwan2 <feel000617@gmail.com>
2023-08-01 18:47:07 +0900

    [FEAT] url로 엔티티 조회 추가

--- src/main/java/com/example/GANerate/repository/ExampleImageRepository.java
+++ src/main/java/com/example/GANerate/repository/ExampleImageRepository.java
@@ -4,4 +4,5 @@ import com.example.GANerate.domain.ExampleImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExampleImageRepository extends JpaRepository<ExampleImage, Long> {
+    ExampleImage findByImageUrl(String imageUrl);
 }
--- src/main/java/com/example/GANerate/repository/ZipFileRepository.java
+++ src/main/java/com/example/GANerate/repository/ZipFileRepository.java
@@ -4,4 +4,5 @@ import com.example.GANerate.domain.ZipFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ZipFileRepository extends JpaRepository<ZipFile, Long> {
+    ZipFile findByUploadUrl(String uploadUrl);
 }
