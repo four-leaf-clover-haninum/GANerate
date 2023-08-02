00f313c2287ae38e4fe04b30b6eb843c3837e593
thisishwan2 <feel000617@gmail.com>
2023-08-02 15:10:17 +0900

    [FEAT] 주석처리

--- src/test/java/com/example/GANerate/GaNerateApplicationTests.java
+++ src/test/java/com/example/GANerate/GaNerateApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.GANerate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GaNerateApplicationTests {
 
 	@Test
