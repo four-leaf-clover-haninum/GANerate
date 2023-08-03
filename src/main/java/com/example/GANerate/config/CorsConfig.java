bbedf13cefebe62925d8d589a48628b929401e97
thisishwan2 <feel000617@gmail.com>
2023-08-03 14:07:07 +0900

    [FEAT] 프론트 CORS

--- src/main/java/com/example/GANerate/config/CorsConfig.java
+++ src/main/java/com/example/GANerate/config/CorsConfig.java
@@ -11,11 +11,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://210.94.182.28:3000", "http://localhost:8080", "http://3.35.255.4:8080", "http://3.35.255.4:8081", "http://3.35.255.4:80", "http://3.35.255.4:8082")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://3.35.255.4:8080", "http://3.35.255.4:8081", "http://3.35.255.4:80", "http://3.35.255.4:8082")
 //                .exposedHeaders("Authorization") // 'Authorization' 헤더 값을 받아온다
                 .allowedMethods(
                         "GET","POST","PUT","PATCH","DELETE","OPTIONS"
                 )
+                //"http://210.94.182.28:3000"
                 .allowCredentials(true);
     }
 }
