616024e3d79241da923749f227b51cf6bcda590d
thisishwan2 <feel000617@gmail.com>
2023-08-04 14:01:50 +0900

    [FIX] CORS 수정

--- src/main/java/com/example/GANerate/config/CorsConfig.java
+++ src/main/java/com/example/GANerate/config/CorsConfig.java
@@ -11,7 +11,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://3.35.255.4:8080", "http://3.35.255.4:8081", "http://3.35.255.4:80", "http://3.35.255.4:8082")
+
+                //이후에 프론트 ec2의 아이피로 아래의 3000번을 변경.
+//                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://3.35.255.4:8080", "http://3.35.255.4:8081", "http://3.35.255.4:80", "http://3.35.255.4:8082")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://3.35.255.4") //혹은 http://3.35.255.4:80 이것 까지 열어보기
+
 //                .exposedHeaders("Authorization") // 'Authorization' 헤더 값을 받아온다
                 .allowedMethods(
                         "GET","POST","PUT","PATCH","DELETE","OPTIONS"
