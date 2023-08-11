cf2bd2984b7cb411e05b389cd6bd416b499cf09c
thisishwan2 <feel000617@gmail.com>
2023-08-11 16:11:39 +0900

    [FEAT] 주문 응답 DTO추가

---
+++ src/main/java/com/example/GANerate/response/order/OrderResponse.java
@@ -0,0 +1,46 @@
+package com.example.GANerate.response.order;
+
+import com.example.GANerate.domain.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+public class OrderResponse {
+
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ProductBuyOrder{
+        private Long userId;
+        private String userName;
+        private String userEmail;
+        private Long dataProductId;
+        private String dataProductTitle;
+        private Long dataProductPrice;
+        private List<String> categoryName;
+        private String zipFileOriginalFileName;
+        private double zipFileSizeGb;
+        private Long dataProductSize;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CreateOrder{
+        private String dataProductName;
+        private Long dataProductId;
+        private Long price;
+        private String email;
+        private String userName;
+        private String phoneNum;
+        private Long orderId;
+    }
+
+
+}
