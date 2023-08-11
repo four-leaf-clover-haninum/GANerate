b7c04a53e8e14586a4fc9ce0469acb12528b5577
thisishwan2 <feel000617@gmail.com>
2023-08-11 16:09:09 +0900

    [FEAT] 빌더패턴 수정

--- src/main/java/com/example/GANerate/domain/OrderItem.java
+++ src/main/java/com/example/GANerate/domain/OrderItem.java
@@ -2,11 +2,13 @@ package com.example.GANerate.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class OrderItem {
 
     @Id
@@ -23,13 +25,19 @@ public class OrderItem {
     private DataProduct dataProduct;
 
     @Builder
-    public OrderItem(Long id){
+    public OrderItem(Long id, Order order, DataProduct dataProduct){
         this.id=id;
+        this.order=order;
+        this.dataProduct=dataProduct;
     }
 
     // Order 연관관계 편의 메서드
     public void setOrder(Order order){
         this.order=order;
         order.getOrderItems().add(this);
+    }
+
+    public void setDataProduct(DataProduct dataProduct){
+        this.dataProduct=dataProduct;
     }
 }
