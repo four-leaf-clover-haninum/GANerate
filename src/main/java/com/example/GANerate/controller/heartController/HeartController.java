4641088660eaba589e09d99c3053bfc169723ec6
thisishwan2 <feel000617@gmail.com>
2023-08-11 14:01:39 +0900

    [FIX] 좋아요 취소 수정

--- src/main/java/com/example/GANerate/controller/heartController/HeartController.java
+++ src/main/java/com/example/GANerate/controller/heartController/HeartController.java
@@ -22,11 +22,6 @@ public class HeartController {
         return CustomResponseEntity.success(heartService.like(dataProductId));
     }
 
-//    @PostMapping("/v1/hearts/{data-product-id}")
-//    public CustomResponseEntity<HeartResponse> like(@AuthenticationPrincipal userId, @PathVariable("data-product-id") Long dataProductId) {
-//        return CustomResponseEntity.success(heartService.like(dataProductId));
-//    }
-
     // 좋아요 취소
     @DeleteMapping("/v1/hearts/{data-product-id}")
     public CustomResponseEntity unlike(@PathVariable("data-product-id") Long dataProductId){
