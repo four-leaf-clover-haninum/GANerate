a7c5941f64b9b72ba3e92990a159df0d8aa13527
thisishwan2 <feel000617@gmail.com>
2023-08-11 14:02:39 +0900

    [FEAT] JpaSpecificationExecutor 추가

--- src/main/java/com/example/GANerate/repository/DataProductRepository.java
+++ src/main/java/com/example/GANerate/repository/DataProductRepository.java
@@ -5,12 +5,13 @@ import com.example.GANerate.domain.ProductCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface DataProductRepository extends JpaRepository<DataProduct, Long>{
+public interface DataProductRepository extends JpaRepository<DataProduct, Long>, JpaSpecificationExecutor<DataProduct> {
 
     Page<DataProduct> findAllBy(Pageable pageable);
 
