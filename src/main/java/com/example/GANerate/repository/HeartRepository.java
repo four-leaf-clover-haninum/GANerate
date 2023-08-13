3493e75bf058fb6fe027f30cf73cd9e99d98205c
thisishwan2 <feel000617@gmail.com>
2023-08-14 02:46:11 +0900

    [FEAT] 회원을 통해 좋아요한 상품 조회

--- src/main/java/com/example/GANerate/repository/HeartRepository.java
+++ src/main/java/com/example/GANerate/repository/HeartRepository.java
@@ -4,8 +4,15 @@ import com.example.GANerate.domain.DataProduct;
 import com.example.GANerate.domain.Heart;
 import com.example.GANerate.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
     Heart findByUserAndDataProduct(User user, DataProduct dataProduct);
+
+    @Query("SELECT H.dataProduct FROM Heart H WHERE H.user = :user")
+    Optional<List<DataProduct>> findDataProductsByUser(User user);
 }
