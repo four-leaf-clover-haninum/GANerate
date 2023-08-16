d77ec67ce29e0696fb841db124437687dfc51218
thisishwan2 <feel000617@gmail.com>
2023-08-16 21:51:33 +0900

    [FEAT] 데이터 상품 상세조회 구현

--- src/main/java/com/example/GANerate/service/dataProduct/DataProductSearchService.java
+++ src/main/java/com/example/GANerate/service/dataProduct/DataProductSearchService.java
@@ -2,6 +2,8 @@ package com.example.GANerate.service.dataProduct;
 
 import com.example.GANerate.domain.DataProduct;
 import com.example.GANerate.domain.ProductCategory;
+import com.example.GANerate.enumuration.Result;
+import com.example.GANerate.exception.CustomException;
 import com.example.GANerate.repository.DataProductRepository;
 import com.example.GANerate.repository.DataProductSpecifications;
 import com.example.GANerate.request.dateProduct.DataProductRequest;
@@ -26,42 +28,46 @@ public class DataProductSearchService {
     // 조건 검색 Specification
     @Transactional(readOnly = true)
     public Page<DataProductResponse.findDataProducts> findDataProductsFiltered(DataProductRequest.filter request) {
-        Pageable pageable = PageRequest.of(request.getPage(), 10, Sort.by("createdAt").descending());
-        String title = request.getTitle();
-        Long maxPrice = request.getMaxPrice();
-        Long minPrice = request.getMinPrice();
-        List<Long> categoriesId = request.getCategoriesId();
+        try {
+            Pageable pageable = PageRequest.of(request.getPage(), 10, Sort.by("createdAt").descending());
+            String title = request.getTitle();
+            Long maxPrice = request.getMaxPrice();
+            Long minPrice = request.getMinPrice();
+            List<Long> categoriesId = request.getCategoriesId();
 
-        Specification<DataProduct> titleSpec = DataProductSpecifications.hasTitle(title);
-        Specification<DataProduct> priceSpec = DataProductSpecifications.hasPriceBetween(minPrice, maxPrice);
+            Specification<DataProduct> titleSpec = DataProductSpecifications.hasTitle(title);
+            Specification<DataProduct> priceSpec = DataProductSpecifications.hasPriceBetween(minPrice, maxPrice);
 
-        List<DataProductResponse.findDataProducts> findDataProducts = new ArrayList<>();
-        List<DataProduct> findFilteredAll = dataProductRepository.findAll(Specification.where(titleSpec).and(priceSpec));
-        for (DataProduct dataProduct : findFilteredAll) {
-            List<ProductCategory> productCategories = dataProduct.getProductCategories();
-            List<Long> categoryIds = new ArrayList<>();
-            List<String> categoryNames = new ArrayList<>();
-            for (ProductCategory productCategory : productCategories) {
-                Long categoryId = productCategory.getCategory().getId();
-                String categoryName = productCategory.getCategory().getTitle();
-                categoryIds.add(categoryId);
-                categoryNames.add(categoryName);
+            List<DataProductResponse.findDataProducts> findDataProducts = new ArrayList<>();
+            List<DataProduct> findFilteredAll = dataProductRepository.findAll(Specification.where(titleSpec).and(priceSpec));
+            for (DataProduct dataProduct : findFilteredAll) {
+                List<ProductCategory> productCategories = dataProduct.getProductCategories();
+                List<Long> categoryIds = new ArrayList<>();
+                List<String> categoryNames = new ArrayList<>();
+                for (ProductCategory productCategory : productCategories) {
+                    Long categoryId = productCategory.getCategory().getId();
+                    String categoryName = productCategory.getCategory().getTitle();
+                    categoryIds.add(categoryId);
+                    categoryNames.add(categoryName);
+                }
+                if (categoriesId.isEmpty() || categoryIds.containsAll(categoriesId)) {
+                    DataProductResponse.findDataProducts response = DataProductResponse.findDataProducts.builder()
+                            .dataProductId(dataProduct.getId())
+                            .buyCnt(dataProduct.getBuyCnt())
+                            .title(dataProduct.getTitle())
+                            .price(dataProduct.getPrice())
+                            .description(dataProduct.getDescription())
+                            .imageUrl(dataProduct.getExampleImages().get(0).getImageUrl())
+                            .createdAt(dataProduct.getCreatedAt())
+                            .categoryIds(categoryIds)
+                            .categoryNames(categoryNames)
+                            .build();
+                    findDataProducts.add(response);
+                }
             }
-            if (categoriesId.isEmpty() || categoryIds.containsAll(categoriesId)){
-                DataProductResponse.findDataProducts response = DataProductResponse.findDataProducts.builder()
-                        .id(dataProduct.getId())
-                        .buyCnt(dataProduct.getBuyCnt())
-                        .title(dataProduct.getTitle())
-                        .price(dataProduct.getPrice())
-                        .description(dataProduct.getDescription())
-                        .imageUrl(dataProduct.getExampleImages().get(0).getImageUrl())
-                        .createdAt(dataProduct.getCreatedAt())
-                        .categoryId(categoryIds)
-                        .categoriesName(categoryNames)
-                        .build();
-                findDataProducts.add(response);
-            }
+            return new PageImpl<>(findDataProducts, pageable, findFilteredAll.size());
+        }catch (Exception e) {
+            throw new CustomException(Result.NOT_FOUND_DATA_PRODUCT);
         }
-        return new PageImpl<>(findDataProducts, pageable, findFilteredAll.size());
     }
 }
