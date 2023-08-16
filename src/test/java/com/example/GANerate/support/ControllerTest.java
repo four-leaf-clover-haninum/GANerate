969699bda9fbd02afe81333291b03695a287ed9d
thisishwan2 <feel000617@gmail.com>
2023-08-16 21:47:46 +0900

    [FIX] 컨트롤러 리팩토링에 따른 테스트 리팩토링

--- src/test/java/com/example/GANerate/support/ControllerTest.java
+++ src/test/java/com/example/GANerate/support/ControllerTest.java
@@ -8,14 +8,14 @@ import com.example.GANerate.config.redis.RedisUtil;
 import com.example.GANerate.controller.categoryController.CategoryController;
 import com.example.GANerate.controller.dataProductController.DataProductController;
 import com.example.GANerate.controller.heartController.HeartController;
+import com.example.GANerate.controller.orderController.OrderController;
 import com.example.GANerate.controller.userController.UserController;
-import com.example.GANerate.repository.CategoryRepository;
-import com.example.GANerate.repository.DataProductRepository;
-import com.example.GANerate.repository.HeartRepository;
-import com.example.GANerate.repository.UserRepository;
+import com.example.GANerate.repository.*;
 import com.example.GANerate.service.category.CategoryService;
+import com.example.GANerate.service.dataProduct.DataProductSearchService;
 import com.example.GANerate.service.dataProduct.DataProductService;
 import com.example.GANerate.service.heart.HeartService;
+import com.example.GANerate.service.order.OrderService;
 import com.example.GANerate.service.user.EmailService;
 import com.example.GANerate.service.user.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -32,6 +32,7 @@ import org.springframework.test.web.servlet.MockMvc;
         DataProductController.class,
         CategoryController.class,
         HeartController.class,
+        OrderController.class
 }, excludeAutoConfiguration = SecurityConfig.class)
 public abstract class ControllerTest {
 
@@ -69,6 +70,15 @@ public abstract class ControllerTest {
 
     @MockBean
     protected HeartRepository heartRepository;
+
+    @MockBean
+    protected DataProductSearchService dataProductSearchService;
+
+    @MockBean
+    protected OrderService orderService;
+
+    @MockBean
+    protected OrderRepository orderRepository;
 
     // 아래의 4개는 spring security에서 빈으로 주입해야되는 것들인데, @WebMvcTest는 @Component, @Service, @Repository 를 빈으로 스캔하지 않으므로 @MockBean으로 별도 주입해야함.
 
