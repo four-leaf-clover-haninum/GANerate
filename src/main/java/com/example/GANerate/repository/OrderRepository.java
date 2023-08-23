4ce56cb80c612b6c30f28d5006e9f0bb9d0b3ab4
thisishwan2 <feel000617@gmail.com>
2023-08-23 16:45:13 +0900

    [FEAT] 회원 주문 조회

--- src/main/java/com/example/GANerate/repository/OrderRepository.java
+++ src/main/java/com/example/GANerate/repository/OrderRepository.java
@@ -1,7 +1,18 @@
 package com.example.GANerate.repository;
 
+import com.example.GANerate.domain.DataProduct;
 import com.example.GANerate.domain.Order;
+import com.example.GANerate.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT o FROM Order o WHERE o.user = :user")
+    Optional<List<Order>> findOrdersByUser(User user);
+
+
 }
