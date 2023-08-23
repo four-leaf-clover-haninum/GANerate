5cb5326af371f7ba60fe035c1c0778cbe4720f4f
thisishwan2 <feel000617@gmail.com>
2023-08-23 16:43:41 +0900

    [FIX] 컨트롤러에 서비스 로직이 포함되지 않도록 변경

--- src/main/java/com/example/GANerate/controller/PaymentController.java
+++ src/main/java/com/example/GANerate/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package com.example.GANerate.controller;
 
 import com.example.GANerate.domain.Payment;
+import com.example.GANerate.response.CustomResponseEntity;
+import com.example.GANerate.response.payment.PaymentResponse;
 import com.example.GANerate.service.PaymentService;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
@@ -29,59 +31,17 @@ public class PaymentController {
     private PaymentService paymentService;
 
     /**
-     * impUid로 결제내역 조회.
-     *
-     * @param impUid
-     * @return
-     * @throws IamportResponseException
-     * @throws IOException
-     */
-    public IamportResponse<com.siot.IamportRestClient.response.Payment> paymentLookup(String impUid) throws IamportResponseException, IOException{
-        return iamportClient.paymentByImpUid(impUid);
-    }
-
-    /**
-     * impUid를 결제 번호로 찾고, 조회해야하는 경우.<br>
-     * 오버로딩.
-     *
-     * @return
-     * @throws IamportResponseException
-     * @throws IOException
-     */
-    public IamportResponse<com.siot.IamportRestClient.response.Payment> paymentLookup(Long paymentId) throws IamportResponseException, IOException{
-        Payment payment = paymentService.paymentLookupService(paymentId);
-        return iamportClient.paymentByImpUid(payment.getImpUid());
-    }
-
-    /**
      * 결제검증을 위한 메서드<br>
      * map에는 imp_uid, amount, actionBoardNo 이 키값으로 넘어옴.
-     *
-     * @param map
-     * @return
-     * @throws IamportResponseException
-     * @throws IOException
      */
     @PostMapping("/v1/payments/verifyIamport")
-    public IamportResponse<com.siot.IamportRestClient.response.Payment> verifyIamport(@RequestBody Map<String,String> map) throws IamportResponseException, IOException{
-        log.info("==========");
+    public CustomResponseEntity<PaymentResponse.CreatePayment> verifyIamport(@RequestBody Map<String,String> map) throws IamportResponseException, IOException{
+
         String impUid = map.get("imp_uid");//실제 결제금액 조회위한 아임포트 서버쪽에서 id
         Long dataProductId = Long.parseLong(map.get("dataProductId")); //DB에서 물건 가격 조회를 위한 번호
         Integer amount = Integer.parseInt(map.get("amount"));//실제로 유저가 결제한 금액
 
-        log.info("==========");
-        log.info(impUid);
-
         //아임포트 서버쪽에 결제된 정보 조회.
-        //paymentByImpUid 는 아임포트에 제공해주는 api인 결제내역 조회(/payments/{imp_uid})의 역할을 함.
-        IamportResponse<com.siot.IamportRestClient.response.Payment> irsp = paymentLookup(impUid);
-
-        log.info("==========");
-
-        paymentService.verifyIamportService(irsp, amount, dataProductId);
-        log.info("==========");
-
-        return irsp;
+        return CustomResponseEntity.success(paymentService.verifyIamportService(impUid, amount, dataProductId, iamportClient));
     }
-
 }
